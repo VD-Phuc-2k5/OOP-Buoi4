4ce5b44123f0e5243f6b34ce17e27e3f59b09061
duyvo2005 <duyvo1954@gmail.com>
2025-02-25 11:33:44 +0700

    example for class NhanVien

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace OOP_Buoi04
     internal class Program
     {
 
-        public static void Main(string[] args) { }
+        public static void Main(string[] args) {
+            NhanVien nv1 = new NhanVien("NV001", "Nguyen Van A", 2.34, 2020);
+            nv1.showTT();
+
+            NhanVien.capNhatMLTT(1900);
+
+            NhanVien nv2 = new NhanVien("NV002", "Nguyen Van B", 2.34, 2020);
+            nv2.showTT();
+        }
     }
 }
