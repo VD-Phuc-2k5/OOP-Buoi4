6af1fa89225eef36eb186ea19a8abe27084e6e41
duyvo2005 <duyvo1954@gmail.com>
2025-02-25 10:46:04 +0700

    overload add function

--- PhanSo.cs
+++ PhanSo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,40 @@ namespace OOP_Buoi04
 {
     internal class PhanSo
     {
+        private int tuSo;
+        private int mauSo;
+
+        public PhanSo()
+        {
+            mauSo = 1;
+        }
+
+        public PhanSo(int ts, int ms) {
+            tuSo = ts;
+            mauSo = ms;
+        }
+
+        public PhanSo(PhanSo phanSo)
+        {
+            tuSo = phanSo.tuSo;
+            mauSo = phanSo.mauSo;
+        }
+
+        public PhanSo cong(PhanSo ps)
+        {
+            PhanSo tmp = new PhanSo(); 
+            tmp.tuSo = this.tuSo * ps.mauSo + this.mauSo * ps.tuSo;
+            tmp.mauSo = this.mauSo * ps.mauSo;
+            return tmp;
+        }
+
+        public PhanSo cong(int x)
+        {
+            PhanSo tmp = new PhanSo();
+            tmp.tuSo = this.mauSo * x + this.tuSo;
+            tmp.mauSo = this.mauSo;
+            return tmp;
+        }
+
     }
 }
